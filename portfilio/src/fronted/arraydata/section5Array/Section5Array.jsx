e67b1dde7692350ae25175666f7b0099fdeec9c3
balochkurram761 <balochkhurram102@gmail.com>
2025-05-12 13:02:40 -0700

    add website

--- portfilio/src/fronted/arraydata/section5Array/Section5Array.jsx
+++ portfilio/src/fronted/arraydata/section5Array/Section5Array.jsx
@@ -1,22 +1,28 @@
-const portfolio=([
-    {   link:"https://farmhouse1.vercel.app/",
-        image:"/assets/Section5/swissHeven Farmhouse.PNG",
-        title:"Swiss Haven FarmHouse - Design for Farmhouse Website"
+const portfolio = ([
+    {
+        link: "https://farmhouse1.vercel.app/",
+        image: "/assets/Section5/swissHeven Farmhouse.PNG",
+        title: "Swiss Haven FarmHouse - Design for Farmhouse Website"
     },
     {
-        link:"https://farmhouse2.vercel.app/",
-        image:"/assets/Section5/alnafey1.PNG",
-        title:"AL Nafey Group - Design For Real Estate Website"
+        link: "https://farmhouse2.vercel.app/",
+        image: "/assets/Section5/alnafey1.PNG",
+        title: "AL Nafey Group - Design For Real Estate Website"
     },
     {
-        link:"https://farmhouse-sigma.vercel.app/",
-        image:"/assets/Section5/Quranweb.PNG",
-        title:"QuranArt - Design For learning Website"
+        link: "https://farmhouse-sigma.vercel.app/",
+        image: "/assets/Section5/Quranweb.PNG",
+        title: "QuranArt - Design For learning Website"
     },
     {
-        link:"https://farmhouse-vt2e.vercel.app/sale",
-        image:"/assets/Section5/web.PNG",
-        title:"Khaddi- Design For E commerce Website"
+        link: "https://farmhouse-vt2e.vercel.app/sale",
+        image: "/assets/Section5/web.PNG",
+        title: "Khaddi- Design For E commerce Website"
+    },
+    {
+        link: "https://pizza-pulse-phi.vercel.app/",
+        image: "/assets/Section5/pizzapulse.PNG",
+        title: "Pizza Pulsa- Design For Food Website",
     }
 
 
